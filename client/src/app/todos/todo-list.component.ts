0caf049b426c57c1fb296f4ebbcc5fc7afacabc4
Anton Olson <ols00559@morris.umn.edu>
2025-02-24 19:02:37 -0600

    (WIP) Added support for filtering todos by status
    Within these five files, I added the basis for filtering todos by status. However, these are not complete (i.e., does not work when running the code).

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -1,26 +1,85 @@
-import { Component, Signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Component, signal, computed } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { TodoService } from './todo.service';
 import { Todo } from './todo';
+import { FormsModule } from '@angular/forms';
+import { catchError, combineLatest, of, switchMap, tap } from 'rxjs';
+import { MatFormField, MatHint } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatHint,
+    MatInputModule,
+    FormsModule
+  ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
+  todoStatus = signal<boolean | undefined>(undefined);
+
+  errMsg = signal<string | undefined>(undefined);
   /**
-    * This constructor injects instance of `UserService`
+    * This constructor injects instance of `TodoService`
     * into this component.
-    * `UserService` lets us interact with the server.
+    * `TodoService` lets us interact with the server.
     *
-    * @param todoService the `UserService` used to get users from the server
+    * @param todoService the `TodoService` used to get todos from the server
+      @param snackBar
     */
-    constructor(private todoService: TodoService) {
+
+    constructor(private todoService: TodoService, private snackBar: MatSnackBar) {
       // Nothing here – everything is in the injection parameters.
     }
 
-    todos: Signal<Todo[]> = toSignal(this.todoService.getTodos());
+    private todoStatus$ = toObservable(this.todoStatus);
+
+    serverFilteredTodos =
+    // This `combineLatest` call takes the most recent values from these two observables (both built from
+    // signals as described above) and passes them into the following `.pipe()` call. If either of the
+    // `todoRole` or `todoStatus` signals change (because their text fields get updated), then that will trigger
+    // the corresponding `todoRole$` and/or `todoStatus$` observables to change, which will cause `combineLatest()`
+    // to send a new pair down the pipe.
+    toSignal(
+      combineLatest([this.todoStatus$]).pipe(
+        // `switchMap` maps from one observable to another. In this case, we're taking `role` and `status` and passing
+        // them as arguments to `todoService.getTodos()`, which then returns a new observable that contains the
+        // results.
+        switchMap(([ status ]) =>
+          this.todoService.getTodos({
+            status,
+          })
+        ),
+        // `catchError` is used to handle errors that might occur in the pipeline. In this case `todoService.getTodos()`
+        // can return errors if, for example, the server is down or returns an error. This catches those errors, and
+        // sets the `errMsg` signal, which allows error messages to be displayed.
+        catchError((err) => {
+          if (!(err.error instanceof ErrorEvent)) {
+            this.errMsg.set(
+              `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`
+            );
+          }
+          this.snackBar.open(this.errMsg(), 'OK', { duration: 6000 });
+          // `catchError` needs to return the same type. `of` makes an observable of the same type, and makes the array still empty
+          return of<Todo[]>([]);
+        }),
+        // Tap allows you to perform side effects if necessary
+        tap(() => {
+          // A common side effect is printing to the console.
+          // You don't want to leave code like this in the
+          // production system, but it can be useful in debugging.
+          // console.log('Todos were filtered on the server')
+        })
+      )
+    );
+
+    filteredTodos = computed(() => {
+      const serverFilteredTodos = this.serverFilteredTodos();
+      return this.todoService.filterTodos(serverFilteredTodos, {});
+    });
 
 }
--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -6,15 +6,16 @@ import { Todo } from './todo';
 
 /**
  * Service that provides the interface for getting information
- * about `Users` from the server.
+ * about `Todos` from the server.
  */
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  // The URL for the users part of the server API.
+  // The URL for the todos part of the server API.
   readonly todoUrl: string = `${environment.apiUrl}todos`;
 
+  private readonly statusKey = 'status';
   // The private `HttpClient` is *injected* into the service
   // by the Angular framework. This allows the system to create
   // only one `HttpClient` and share that across all services
@@ -26,27 +27,51 @@ export class TodoService {
   }
 
 /**
-  * Get all the users from the server, filtered by the information
+  * Get all the todos from the server, filtered by the information
   * in the `filters` map.
   *
   *
   * @param _filters a map that allows us to specify a target role, age,
   *  or company to filter by, or any combination of those
-  * @returns an `Observable` of an array of `Users`. Wrapping the array
+  * @returns an `Observable` of an array of `Todos`. Wrapping the array
   *  in an `Observable` means that other bits of of code can `subscribe` to
   *  the result (the `Observable`) and get the results that come back
   *  from the server after a possibly substantial delay (because we're
   *  contacting a remote server over the Internet).
   */
-  getTodos(): Observable<Todo[]> {
+  getTodos(filters?: {status?: boolean}): Observable<Todo[]> {
     // `HttpParams` is essentially just a map used to hold key-value
     // pairs that are then encoded as "?key1=value1&key2=value2&…" in
     // the URL when we make the call to `.get()` below.
-    const httpParams: HttpParams = new HttpParams();
+    let httpParams: HttpParams = new HttpParams();
     // Send the HTTP GET request with the given URL and parameters.
-    // That will return the desired `Observable<User[]>`.
+    // That will return the desired `Observable<Todo[]>`.
+    if (filters) {
+      if (filters.status) {
+        httpParams = httpParams.set(this.statusKey, filters.status.toString());
+      }
+    }
     return this.httpClient.get<Todo[]>(this.todoUrl, {
       params: httpParams,
     });
+  }
+
+  /**
+   * A service method that filters an array of `Todo` using
+   * the specified filters.
+   *
+   * Note that the filters here support partial matches. Since the
+   * matching is done locally we can afford to repeatedly look for
+   * partial matches instead of waiting until we have a full string
+   * to match against.
+   *
+   * @param todos the array of `Todos` that we're filtering
+   * @param filters the map of key-value pairs used for the filtering
+   * @returns an array of `Todos` matching the given filters
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  filterTodos(todos: Todo[], filters: { name?: string; company?: string }): Todo[] { // skipcq: JS-0105
+    const filteredTodos = todos;
+    return filteredTodos;
   }
 }
